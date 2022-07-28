6604e75f0f743bb4b08ba2b880e60ad7ebf8ddd9
AnshumaanBaaghi <ganumumbai067@gmail.com>
2022-07-28 15:55:30 +0530

    Portfolio

---
+++ src/component/Portfolio/Portfolio_data.js
@@ -0,0 +1,70 @@
+// import project from './image/project.png'
+import Nykaa from './image/nykaa.png'
+import InternTheory from './image/InternTheory.png'
+import HindustanTimes from './image/HindustanTimes.png'
+import Youtube from './image/Youtube.png'
+import Sudoku from './image/Sudoku.png'
+import Weather from './image/Weather.png'
+const Portfolio_data = [
+  {
+    id: 1,
+    category: "E-commerce",
+    totalLike: "600",
+    title: "Inspired by Nykaa",
+    image: Nykaa,
+    github: 'https://github.com/AnshumaanBaaghi/Nykaa_Clone',
+    deployed: 'https://incredible-pixie-1cbc60.netlify.app',
+    description: 'Nykaa is an Indian e-commerce company, founded by Falguni Nayar in 2012 and headquartered in Mumbai. Our team tried to make its clone.',
+  },
+  {
+    id: 2,
+    category: "E-Learning",
+    totalLike: "750",
+    title: "Inspired by InternTheory",
+    image: InternTheory,
+    github: 'https://github.com/AnshumaanBaaghi/InternTheory-Clone',
+    deployed: 'https://superlative-sprite-7cfce0.netlify.app',
+    description: 'An internship is a period of work experience offered by an organization for a limited period of time.',
+  },
+  {
+    id: 3,
+    category: "E-Paper",
+    totalLike: "630",
+    title: "Inspired by HindustanTimes",
+    image: HindustanTimes,
+    github: 'https://github.com/AnshumaanBaaghi/HindustanTimes_Clone',
+    deployed: false,
+    description: 'Hindustan Times is an Indian English-language daily newspaper founded in 1924 by Sunder Singh Lyallpuri, founder-father of the Akali movement and the Shiromani Akali Dal in the Punjab Province.',
+  },
+  {
+    id: 4,
+    category: "social media",
+    totalLike: "360",
+    title: "Inspired by Youtube",
+    image: Youtube,
+    github: 'https://github.com/AnshumaanBaaghi/Youtube_Clone',
+    deployed: 'https://suspicious-brown-afca5b.netlify.app',
+    description: 'YouTube is an American online video sharing and social media platform headquartered in San Bruno, California. It was launched on February 14, 2005, by Steve Chen, Chad Hurley, and Jawed Karim.',
+  },
+  {
+    id: 5,
+    category: "Game",
+    totalLike: "280",
+    title: "Sudoku",
+    image: Sudoku,
+    github: 'https://github.com/AnshumaanBaaghi/Sudoku',
+    deployed: 'https://zippy-donut-a86b39.netlify.app',
+    description: 'Sudoku  is a logic-based, combinatorial number-placement puzzle. The puzzle setter provides a partially completed grid, which for a well-posed puzzle has a single solution.',
+  },
+  {
+    id: 6,
+    category: "weather forecast",
+    totalLike: "690",
+    title: "Weather App",
+    image: Weather,
+    github: 'https://github.com/AnshumaanBaaghi/WeatherApp',
+    deployed: 'https://epic-bohr-2d7395.netlify.app',
+    description: 'Weather forecasting is the application of science and technology to predict the conditions of the atmosphere for a given location and time.',
+  },
+]
+export default Portfolio_data
